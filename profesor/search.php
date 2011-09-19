4503e549454d87c9ef43d19d57a792b84d93e1f9
antonioformatic <avaraujo@yahoo.es>
2011-09-19 10:56:17 +0200

    Agrego algunos comentarios

--- profesor/search.php
+++ profesor/search.php
@@ -1,4 +1,11 @@
 <?php
+/*
+Busca en una tabla un valor dado y devuelve un array 
+en formato json con los registros en los que el campo
+a buscar contiene el valor dado. Para cada registro 
+encontrado, devuelve el campo a buscar como "label" y
+el campo a devolver como "ret".
+*/
 error_reporting(E_ALL | E_STRICT);
 ini_set('display_errors', 1);
 require_once ('funciones.php');
